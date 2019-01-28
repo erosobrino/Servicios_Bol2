e1fa246e8e80de462b4a50c2b5d26d777c5804e1
Ero Sobrino <ero.sobrino.dorado@gmail.com>
2019-01-28 12:24:57 +0100

    28/1/19

---
+++ Ejer5/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Ejer5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.tbWord = new System.Windows.Forms.TextBox();
+            this.tbFind = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chCapital = new System.Windows.Forms.CheckBox();
+            this.btFind = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(108, 12);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(555, 22);
+            this.tbPath.TabIndex = 0;
+            // 
+            // tbWord
+            // 
+            this.tbWord.Location = new System.Drawing.Point(108, 47);
+            this.tbWord.Name = "tbWord";
+            this.tbWord.Size = new System.Drawing.Size(555, 22);
+            this.tbWord.TabIndex = 1;
+            // 
+            // tbFind
+            // 
+            this.tbFind.Location = new System.Drawing.Point(12, 130);
+            this.tbFind.Multiline = true;
+            this.tbFind.Name = "tbFind";
+            this.tbFind.Size = new System.Drawing.Size(651, 308);
+            this.tbFind.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Word";
+            // 
+            // chCapital
+            // 
+            this.chCapital.AutoSize = true;
+            this.chCapital.Location = new System.Drawing.Point(15, 94);
+            this.chCapital.Name = "chCapital";
+            this.chCapital.Size = new System.Drawing.Size(73, 21);
+            this.chCapital.TabIndex = 5;
+            this.chCapital.Text = "Capital";
+            this.chCapital.UseVisualStyleBackColor = true;
+            // 
+            // btFind
+            // 
+            this.btFind.Location = new System.Drawing.Point(108, 82);
+            this.btFind.Name = "btFind";
+            this.btFind.Size = new System.Drawing.Size(118, 42);
+            this.btFind.TabIndex = 6;
+            this.btFind.Text = "Find";
+            this.btFind.UseVisualStyleBackColor = true;
+            this.btFind.Click += new System.EventHandler(this.btFind_Click);
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(245, 94);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 17);
+            this.lblError.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btFind;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 450);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btFind);
+            this.Controls.Add(this.chCapital);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbFind);
+            this.Controls.Add(this.tbWord);
+            this.Controls.Add(this.tbPath);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Word Finder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.TextBox tbWord;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btFind;
+        private System.Windows.Forms.Label lblError;
+        public System.Windows.Forms.TextBox tbFind;
+        public System.Windows.Forms.CheckBox chCapital;
+    }
+}
+
---
+++ Ejer5/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejer5
+{
+    public partial class Form1 : Form
+    {
+        Thread thread;
+        delegate void DelegateText(string text, TextBox t);
+        public Form1()
+        {
+            InitializeComponent();
+            tbPath.Text = "D:/";
+            tbWord.Text = "a";
+        }
+
+        List<string> textFiles = new List<string>();
+
+        private void btFind_Click(object sender, EventArgs e)
+        {
+            lblError.Text = "";
+            if (tbPath.Text.Length > 0 && tbWord.Text.Length > 0)
+            {
+                DirectoryInfo directory = null;
+                try
+                {
+                    directory = new DirectoryInfo(tbPath.Text);
+                }
+                catch (ArgumentException)
+                {
+                    lblError.Text = "The directory is invalid";
+                }
+                for (int i = 0; i < directory.GetFiles().Length; i++)
+                {
+                    if (directory.GetFiles()[i].Extension.Equals(".txt"))
+                    {
+                        textFiles.Add(directory.GetFiles()[i].FullName);
+                    }
+                }
+                foreach (string str in textFiles)
+                {
+                    thread = new Thread(() => ThreadFind(str, tbWord.Text, chCapital.Checked));
+                    thread.Start();
+                }
+            }
+            else
+            {
+                lblError.Text = "Error in the Path or the Word";
+            }
+        }
+
+        private void changeText(string text, TextBox textBox)
+        {
+            textBox.AppendText(text + Environment.NewLine);
+        }
+
+        private void ThreadFind(string path, string word, bool capital)
+        {
+            int number = 0;
+            //try
+            //{
+            Console.WriteLine(path);
+            using (StreamReader reader = new StreamReader(path))
+            {
+                String line = reader.ReadLine();
+                while (line != null)
+                {
+                    //long b = "hola como estas".LongCount(letra => letra.ToString() == "a");
+                    //number = (int)line.LongCount(worddd => worddd.ToString() == word);
+                    ////if (line.Contains(word))
+                    ////{
+                    ////    number++;
+                    ////}
+                    DelegateText delegateText = new DelegateText(changeText);
+                    this.Invoke(delegateText, line, tbFind);
+                    line = reader.ReadLine();
+                }
+            }
+            //}
+            //catch { }
+        }
+    }
+}
+//string directory = Environment.GetEnvironmentVariable("homedrive") + "\\" + Environment.GetEnvironmentVariable("homepath") + "\\configNotas.bin";
